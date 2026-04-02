3e2fbc2ce6920dcaaa6abaf6e0ae4244113e7e5e
aw3s <andreashwolters@gmail.com>
2026-04-02 15:13:03 +0200

    .

--- main.ts
+++ main.ts
@@ -66,10 +66,18 @@ if (import.meta.main) {
 
   console.log(df.schema);
 
+  console.log(df.filter(pl.col("OBJTYPE").isNull()).toString());
+
+  console.log(df.filter(
+  pl.col("OBJTYPE")
+    .cast(pl.Utf8)
+    .isNull()).toString() // cast fails to Utf8 for non-strings -> null
+);
+
   df = df.withColumns(
   pl.when(
     pl.col('NOfItems').gt(1).or(
-      pl.col('OBJTYPE_PK').str.lengths().lt(3).and(pl.col('OBJTYPE').str.startsWith("ALLG"))
+      pl.col('OBJTYPE_PK').str.lengths().lt(3).and(pl.col('OBJTYPE').cast(pl.Utf8).str.starts_with("ALLG"))
     )
   )
   .then(pl.lit(true))
@@ -78,8 +86,82 @@ if (import.meta.main) {
 );
 console.log(df.toString());
 
-console.log(df.filter(pl.col('OBJTYPE').str.startsWith("ALLG")).select(["OBJTYPE", "OBJTYPE_PK","NOfItems"]).toString());
+//console.log(df.filter(pl.col('OBJTYPE').cast(pl.Utf8).eq("ALLG")).select(["OBJTYPE", "OBJTYPE_PK","NOfItems"]).toString());
 
 
 
 }
+
+
+// PS C:\Users\wolters> mkdir pnhjs
+
+
+//     Verzeichnis: C:\Users\wolters
+
+
+// Mode                 LastWriteTime         Length Name
+// ----                 -------------         ------ ----
+// d-----        29.03.2026     14:31                pnhjs
+
+
+// PS C:\Users\wolters> cd .\pnhjs\
+// PS C:\Users\wolters\pnhjs> deno init
+// ✅ Project initialized
+
+// Run these commands to get started
+
+//   # Run the program
+//   deno run main.ts
+
+//   # Run the program and watch for file changes
+//   deno task dev
+
+//   # Run the tests
+//   deno test
+// PS C:\Users\wolters\pnhjs> dir
+
+
+//     Verzeichnis: C:\Users\wolters\pnhjs
+
+
+// Mode                 LastWriteTime         Length Name
+// ----                 -------------         ------ ----
+// -a----        29.03.2026     14:31            118 deno.json
+// -a----        29.03.2026     14:31            226 main.ts
+// -a----        29.03.2026     14:31            143 main_test.ts
+
+
+// PS C:\Users\wolters\pnhjs> git init
+// Initialized empty Git repository in C:/Users/wolters/pnhjs/.git/
+// PS C:\Users\wolters\pnhjs> git remote add origin https://github.com/KIAWx/pnhjs.git
+// PS C:\Users\wolters\pnhjs> git add .
+// warning: in the working copy of 'deno.json', LF will be replaced by CRLF the next time Git touches it
+// warning: in the working copy of 'main.ts', LF will be replaced by CRLF the next time Git touches it
+// warning: in the working copy of 'main_test.ts', LF will be replaced by CRLF the next time Git touches it
+// PS C:\Users\wolters\pnhjs> git commit -m "Initial commit"
+// [master (root-commit) 977a4c7] Initial commit
+//  3 files changed, 22 insertions(+)
+//  create mode 100644 deno.json
+//  create mode 100644 main.ts
+//  create mode 100644 main_test.ts
+// PS C:\Users\wolters\pnhjs> git branch -M main
+// PS C:\Users\wolters\pnhjs> git push -u origin main
+// remote: Permission to KIAWx/pnhjs.git denied to aw3s.
+// fatal: unable to access 'https://github.com/KIAWx/pnhjs.git/': The requested URL returned error: 403
+// PS C:\Users\wolters\pnhjs> git remote set-url origin https://KIAWx@github.com/KIAWx/pnhjs.git
+// PS C:\Users\wolters\pnhjs> git push -u origin main
+// git: 'credential-nothing' is not a git command. See 'git --help'.
+// Enumerating objects: 5, done.
+// Counting objects: 100% (5/5), done.
+// Delta compression using up to 12 threads
+// Compressing objects: 100% (5/5), done.
+// Writing objects: 100% (5/5), 659 bytes | 109.00 KiB/s, done.
+// Total 5 (delta 0), reused 0 (delta 0), pack-reused 0
+// To https://github.com/KIAWx/pnhjs.git
+//  * [new branch]      main -> main
+// branch 'main' set up to track 'origin/main'.
+// PS C:\Users\wolters\pnhjs> net use /persistent:no M: \\NAS1\AM NASPython3# /user:WOL
+// Der Befehl wurde erfolgreich ausgeführt.
+
+// PS C:\Users\wolters\pnhjs>
+
